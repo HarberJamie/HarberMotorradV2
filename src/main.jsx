c596bd5b822794560b4b85a8337df95e87fea0db
Jamie Fitzsimmons <jamie.fitzsimmons@outlook.com>
2025-10-20 20:54:55 +0100

    Fix build: use relative CSS import and ensure lowercase path

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
 import App from './App.jsx'
-import '@/assets/css/style.css'  // if you use this alias; otherwise use relative path
+import './assets/css/style.css'  // if you use this alias; otherwise use relative path
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
