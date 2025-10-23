bada6d03d63670c348df1a20008dd5f5470b6295
Jamie Fitzsimmons <jamie.fitzsimmons@outlook.com>
2025-10-23 20:46:11 +0100

    Use env-based Router (HashRouter in prod, BrowserRouter in dev)

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, HashRouter } from 'react-router-dom'
 import App from './App.jsx'
-import '@/assets/css/style.css'  // if you use this alias; otherwise use relative path
+import '@/assets/css/style.css'  // keep if '@' alias is configured
 
 const Router = import.meta.env.MODE === 'production' ? HashRouter : BrowserRouter
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HashRouter>
+    <Router>
       <App />
-    </HashRouter>
+    </Router>
   </React.StrictMode>
 )
